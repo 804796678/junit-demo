402c8b0a4ff59a0577b16f3cf8f0e3c7838805d8
yidongwu <wj196868@gmail.com>
2018-09-09 16:29:33 -0400

    test: added ArithmeticSolver test case

---
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -0,0 +1,10 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class ArithmeticSolverTest extends TestCase{
+	
+	public void testAdd() {
+		ArithmeticSolver solver = new ArithmeticSlover();
+	}
+}
