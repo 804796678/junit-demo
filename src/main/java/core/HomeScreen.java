c768f2e184d36a0c1f6a58ab6ff26b34a6a24060
yidongwu <wj196868@gmail.com>
2018-09-11 10:55:17 -0400

    main: add cards animation and click and log

--- src/main/java/core/HomeScreen.java
+++ src/main/java/core/HomeScreen.java
@@ -1,27 +1,53 @@
 package core;
- import javafx.application.Application;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.Random;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
  public class HomeScreen extends Application {
 	
-	private Button solnBtn;
-	private TextField leftOperandTxtBox;
-	private TextField rightOperandTxtBox;
-	private TextField answerTxtBox;
-	private ComboBox<String> operatorDropdown;
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	 private static final Logger logger = LogManager.getLogger(HomeScreen.class);
+		
+		private Button solnBtn;
+		private TextField leftOperandTxtBox;
+		private TextField rightOperandTxtBox;
+		private TextField answerTxtBox;
+		private ComboBox<String> operatorDropdown;
+		private Image[] cardsImg;
+		private ImageView imgView;
+		
+		public static void main(String[] args) {
+			logger.info("Home Screen booting up ...");
+			
+			launch(args);
+		}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -32,13 +58,76 @@ import javafx.stage.Stage;
 		//canvas.setStyle("-fx-background-color: black");
 		
 		addControlsToCanvas(canvas);
+		setupCardAnimation(canvas);
 		
-		Scene scene = new Scene(canvas, 320, 200);
+		Scene scene = new Scene(canvas, 320, 500);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Simple Calculator App");
 		primaryStage.show();
 	}
- 	private void addControlsToCanvas(Pane canvas) {
+ 	
+ 	private void setupCardAnimation(Pane canvas) {
+		File cardsDir = new File("src/main/resources/Card");
+		FilenameFilter imgFilter = new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				// TODO Auto-generated method stub
+				return name.toLowerCase().endsWith("png");
+			}
+		};
+		
+		File[] cardsFile = cardsDir.listFiles(imgFilter);
+		
+		cardsImg = new Image[cardsFile.length];
+		int idx = 0;
+		
+		for (File cardFile : cardsFile) {
+			try {
+				cardsImg[idx] = new Image(new FileInputStream(cardFile.getPath()));
+				idx++;
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		imgView = new ImageView();
+		imgView.setImage(cardsImg[0]);
+		imgView.relocate(20, 120);
+		imgView.setFitWidth(150);
+		imgView.setFitHeight(200);
+		imgView.setPreserveRatio(true);
+		
+		Timeline timeline = new Timeline();
+		timeline.setAutoReverse(true);
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		
+		KeyValue keyValue = new KeyValue(imgView.xProperty(), 200, Interpolator.EASE_BOTH);
+		KeyFrame keyFrame = new KeyFrame(Duration.millis(800), keyValue);
+		
+		timeline.getKeyFrames().add(keyFrame);
+		timeline.play();
+		
+		setCardClickHandler();
+				
+		canvas.getChildren().addAll(imgView);
+	}
+ 	
+ 	private void setCardClickHandler() {
+		Random rand = new Random();
+		
+		imgView.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			logger.info("Card click detected");
+			Image randomImage = cardsImg[rand.nextInt(cardsImg.length)];
+			imgView.setImage(randomImage);
+		});
+	}
+ 	
+ 	
+ 	
+ 	
+ 	
+	private void addControlsToCanvas(Pane canvas) {
 		int row1 = 20;
 		int row2 = 60;
 		int txtBoxWidth = 50;
